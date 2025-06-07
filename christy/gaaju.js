d3c4eeb7b05f3e3c1bfd17beea1931e6139c684e
Chris Gaaju <chimbikouzukwu202@gmail.com>
2025-06-08 00:24:34 +0100

    Create gaaju.js

---
+++ christy/gaaju.js
@@ -0,0 +1,17 @@
+
+var tabCmds = [];
+let cm = [];
+function gaaju(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "⚡";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { gaaju, Module: gaaju, cm };
